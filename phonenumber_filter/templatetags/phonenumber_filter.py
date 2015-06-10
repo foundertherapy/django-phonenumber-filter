6f3bf47ec710a6dd9fce068374f9328c3fa632f9
Dana Spiegel <dana@sociabledesign.com>
2015-06-10 12:16:48 -0400

    added tests and more filters for international and e164 formats made US the default country

--- phonenumber_filter/templatetags/phonenumber_filter.py
+++ phonenumber_filter/templatetags/phonenumber_filter.py
@@ -1,5 +1,26 @@
+from django import template
+register = template.Library()
+
 import phonenumbers
 
+
 @register.filter(name='phonenumber')
-def phonenumber(value, country=None):
-    return phonenumbers.parse(value, country)
+def phonenumber(
+        value, country='US', format=phonenumbers.PhoneNumberFormat.NATIONAL):
+    try:
+        parsed = phonenumbers.parse(value, country)
+        return phonenumbers.format_number(parsed, format)
+    except phonenumbers.NumberParseException as e:
+        return value
+        
+
+@register.filter(name='phonenumber_intl')
+def phonenumber_international(value, country='US'):
+    return phonenumber(
+        value, country, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+
+
+@register.filter(name='phonenumber_e164')
+def phonenumber_e164(value, country='US'):
+    return phonenumber(
+        value, country, phonenumbers.PhoneNumberFormat.E164)
---
+++ phonenumber_filter/tests.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+import unittest
+
+from templatetags.phonenumber_filter import (
+    phonenumber, phonenumber_international, phonenumber_e164)
+
+
+class PhoneNumberFilterTestCase(unittest.TestCase):
+    def test_bad_phonenumber(self):
+        self.assertEqual('123', phonenumber('123'))
+        self.assertEqual('', phonenumber(''))
+        self.assertEqual('abc', phonenumber('abc'))
+        self.assertEqual('222125551212', phonenumber('222125551212'))
+
+    def test_phonenumber(self):
+        self.assertEqual('(212) 867-5309', phonenumber('2128675309'))
+        self.assertEqual('(212) 867-5309', phonenumber('12128675309'))
+
+    def test_phonenumber_international(self):
+        self.assertEqual('+1 212-867-5309', 
+                         phonenumber_international('2128675309'))
+        self.assertEqual('+1 212-867-5309', 
+                         phonenumber_international('12128675309'))
+
+    def test_phonenumber_e164(self):
+        self.assertEqual('+12128675309', phonenumber_e164('2128675309'))
+        self.assertEqual('+12128675309', phonenumber_e164('12128675309'))
+
